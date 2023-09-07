13e5483d367a53c2d36ca721cac87bd52ed08d63
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-09-07 15:59:23 +0300

    fix: incompatible dependency for edge

--- server/api/users/index.post.ts
+++ server/api/users/index.post.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from '#auth'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 import { db } from '../../db/service'
 import { users, InsertUser } from '../../db/schema'
