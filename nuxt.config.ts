74299aaf70de28aae75f0b0f573ea490bd6a49c9
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-09-11 13:55:25 +0300

    feat: add nuxt/ui package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@sidebase/nuxt-auth'],
+  modules: ['@sidebase/nuxt-auth', '@nuxt/ui'],
   devtools: { enabled: true },
   auth: {
     globalAppMiddleware: true
+  },
+  ui: {
+    global: true
   },
   runtimeConfig: {
     publicRoutes: process.env.PUBLIC_ROUTES?.split(',') || []
